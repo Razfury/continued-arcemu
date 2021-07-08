112219c0a7e04c70dc9e453a2b7e22e952e8f633
Razfury <wolcronic@hotmail.com>
2021-07-08 14:20:01 -0400

    [Core/Spells] Fix max targets for a bunch of spells.

--- src/world/Game/Spell/HackFixes.cpp
+++ src/world/Game/Spell/HackFixes.cpp
@@ -6178,6 +6178,197 @@ void ApplyNormalFixes()
             sp->AreaAuraTarget = SPELL_EFFECT_APPLY_RAID_AREA_AURA;
         }break;
 
+        //Creature max targets fixes.
+        case 802:                                   // Mutate Bug (AQ40, Emperor Vek'nilash)
+        case 804:                                   // Explode Bug (AQ40, Emperor Vek'lor)
+        case 23138:                                 // Gate of Shazzrah (MC, Shazzrah)
+        case 28560:                                 // Summon Blizzard (Naxx, Sapphiron)
+        case 30541:                                 // Blaze (Magtheridon)
+        case 30572:                                 // Quake (Magtheridon)
+        case 30769:                                 // Pick Red Riding Hood (Karazhan, Big Bad Wolf)
+        case 30835:                                 // Infernal Relay (Karazhan, Prince Malchezaar)
+        case 31347:                                 // Doom (Hyjal Summit, Azgalor)
+        case 32312:                                 // Move 1 (Karazhan, Chess Event)
+        case 33711:                                 // Murmur's Touch (Shadow Labyrinth, Murmur)
+        case 37388:                                 // Move 2 (Karazhan, Chess Event)
+        case 38794:                                 // Murmur's Touch (h) (Shadow Labyrinth, Murmur)
+        case 39338:                                 // Karazhan - Chess, Medivh CHEAT: Hand of Medivh, Target Horde
+        case 39342:                                 // Karazhan - Chess, Medivh CHEAT: Hand of Medivh, Target Alliance
+        case 40834:                                 // Agonizing Flames (BT, Illidan Stormrage)
+        case 41537:                                 // Summon Enslaved Soul (BT, Reliquary of Souls)
+        case 44869:                                 // Spectral Blast (SWP, Kalecgos)
+        case 45391:                                 // Summon Demonic Vapor (SWP, Felmyst)
+        case 45785:                                 // Sinister Reflection Clone (SWP, Kil'jaeden)
+        case 45863:                                 // Cosmetic - Incinerate to Random Target (Borean Tundra)
+        case 45892:                                 // Sinister Reflection (SWP, Kil'jaeden)
+        case 45976:                                 // Open Portal (SWP, M'uru)
+        case 46372:                                 // Ice Spear Target Picker (Slave Pens, Ahune)
+        case 47669:                                 // Awaken Subboss (Utgarde Pinnacle)
+        case 48278:                                 // Paralyze (Utgarde Pinnacle)
+        case 50742:                                 // Ooze Combine (Halls of Stone)
+        case 50988:                                 // Glare of the Tribunal (Halls of Stone)
+        case 51003:                                 // Summon Dark Matter Target (Halls of Stone)
+        case 51146:                                 // Summon Searing Gaze Target (Halls Of Stone)
+        case 52438:                                 // Summon Skittering Swarmer (Azjol Nerub,  Krik'thir the Gatewatcher)
+        case 52449:                                 // Summon Skittering Infector (Azjol Nerub,  Krik'thir the Gatewatcher)
+        case 53457:                                 // Impale (Azjol Nerub,  Anub'arak)
+        case 54148:                                 // Ritual of the Sword (Utgarde Pinnacle, Svala)
+        case 55479:                                 // Forced Obedience (Naxxramas, Razovius)
+        case 56140:                                 // Summon Power Spark (Eye of Eternity, Malygos)
+        case 57578:                                 // Lava Strike (Obsidian Sanctum, Sartharion)
+        case 59870:                                 // Glare of the Tribunal (h) (Halls of Stone)
+        case 62016:                                 // Charge Orb (Ulduar, Thorim)
+        case 62042:                                 // Stormhammer (Ulduar, Thorim)
+        case 62166:                                 // Stone Grip (Ulduar, Kologarn)
+        case 62301:                                 // Cosmic Smash (Ulduar, Algalon)
+        case 62374:                                 // Pursued (Ulduar, Flame Leviathan)
+        case 62488:                                 // Activate Construct (Ulduar, Ignis)
+        case 62577:                                 // Blizzard (Ulduar, Thorim)
+        case 62603:                                 // Blizzard (h) (Ulduar, Thorim)
+        case 62797:                                 // Storm Cloud (Ulduar, Hodir)
+        case 62978:                                 // Summon Guardian (Ulduar, Yogg Saron)
+        case 63018:                                 // Searing Light (Ulduar, XT-002)
+        case 63024:                                 // Gravity Bomb (Ulduar, XT-002)
+        case 63545:                                 // Icicle (Ulduar, Hodir)
+        case 63744:                                 // Sara's Anger (Ulduar, Yogg-Saron)
+        case 63745:                                 // Sara's Blessing (Ulduar, Yogg-Saron)
+        case 63747:                                 // Sara's Fervor (Ulduar, Yogg-Saron)
+        case 63795:                                 // Psychosis (Ulduar, Yogg-Saron)
+        case 63820:                                 // Summon Scrap Bot Trigger (Ulduar, Mimiron) use for Scrap Bots, hits npc 33856
+        case 63830:                                 // Malady of the Mind (Ulduar, Yogg-Saron)
+        case 64218:                                 // Overcharge (VoA, Emalon)
+        case 64234:                                 // Gravity Bomb (h) (Ulduar, XT-002)
+        case 64402:                                 // Rocket Strike (Ulduar, Mimiron)
+        case 64425:                                 // Summon Scrap Bot Trigger (Ulduar, Mimiron) use for Assault Bots, hits npc 33856
+        case 64465:                                 // Shadow Beacon (Ulduar, Yogg-Saron)
+        case 64543:                                 // Melt Ice (Ulduar, Hodir)
+        case 64623:                                 // Frost Bomb (Ulduar, Mimiron)
+        case 65121:                                 // Searing Light (h) (Ulduar, XT-002)
+        case 65301:                                 // Psychosis (Ulduar, Yogg-Saron)
+        case 65872:                                 // Pursuing Spikes (ToCrusader, Anub'arak)
+        case 65950:                                 // Touch of Light (ToCrusader, Val'kyr Twins)
+        case 66001:                                 // Touch of Darkness (ToCrusader, Val'kyr Twins)
+        case 66152:                                 // Bullet Controller Summon Periodic Trigger Light (ToCrusader)
+        case 66153:                                 // Bullet Controller Summon Periodic Trigger Dark (ToCrusader)
+        case 66332:                                 // Nerubian Burrower (Mode 0) (ToCrusader, Anub'arak)
+        case 66336:                                 // Mistress' Kiss (ToCrusader, Jaraxxus)
+        case 66339:                                 // Summon Scarab (ToCrusader, Anub'arak)
+        case 67077:                                 // Mistress' Kiss (Mode 2) (ToCrusader, Jaraxxus)
+        case 67281:                                 // Touch of Darkness (Mode 1)
+        case 67282:                                 // Touch of Darkness (Mode 2)
+        case 67283:                                 // Touch of Darkness (Mode 3)
+        case 67296:                                 // Touch of Light (Mode 1)
+        case 67297:                                 // Touch of Light (Mode 2)
+        case 67298:                                 // Touch of Light (Mode 3)
+        case 68912:                                 // Wailing Souls (FoS)
+        case 68950:                                 // Fear (FoS)
+        case 68987:                                 // Pursuit (PoS)
+        case 69048:                                 // Mirrored Soul (FoS)
+        case 69057:                                 // Bone Spike Graveyard (Icecrown Citadel, Lord Marrowgar) 10 man
+        case 72088:
+        case 73142:
+        case 73144:
+        case 69140:                                 // Coldflame (ICC, Marrowgar)
+        case 69674:                                 // Mutated Infection (ICC, Rotface)
+        case 70450:                                 // Blood Mirror
+        case 70837:                                 // Blood Mirror
+        case 70882:                                 // Slime Spray Summon Trigger (ICC, Rotface)
+        case 70920:                                 // Unbound Plague Search Effect (ICC, Putricide)
+        case 71224:                                 // Mutated Infection (Mode 1)
+        case 71445:                                 // Twilight Bloodbolt
+        case 71471:                                 // Twilight Bloodbolt
+        case 71837:                                 // Vampiric Bite
+        case 71861:                                 // Swarming Shadows
+        case 72091:                                 // Frozen Orb (Vault of Archavon, Toravon)
+        case 72254:                                 // Mark of Fallen Champion (target selection) (ICC, Deathbringer Saurfang)
+        case 73022:                                 // Mutated Infection (Mode 2)
+        case 73023:                                 // Mutated Infection (Mode 3)
+            sp->MaxTargets = 1;
+            break;
+        case 10258:                                 // Awaken Vault Warder (Uldaman)
+        case 28542:                                 // Life Drain (Naxx, Sapphiron)
+        case 62476:                                 // Icicle (Ulduar, Hodir)
+        case 63802:                                 // Brain Link (Ulduar, Yogg-Saron)
+        case 66013:                                 // Penetrating Cold (10 man) (ToCrusader, Anub'arak)
+        case 67755:                                 // Nerubian Burrower (Mode 1) (ToCrusader, Anub'arak)
+        case 67756:                                 // Nerubian Burrower (Mode 2) (ToCrusader, Anub'arak)
+        case 68509:                                 // Penetrating Cold (10 man heroic)
+        case 69055:                                 // Bone Slice (ICC, Lord Marrowgar)
+        case 69278:                                 // Gas spore (ICC, Festergut)
+        case 70341:                                 // Slime Puddle (ICC, Putricide)
+        case 71336:                                 // Pact of the Darkfallen
+        case 71390:                                 // Pact of the Darkfallen
+            sp->MaxTargets = 2;
+            break;
+        case 28796:                                 // Poison Bolt Volley (Naxx, Faerlina)
+        case 29213:                                 // Curse of the Plaguebringer (Naxx, Noth the Plaguebringer)
+        case 30004:                                 // Flame Wreath (Karazhan, Shade of Aran)
+        case 31298:                                 // Sleep (Hyjal Summit, Anetheron)
+        case 39341:                                 // Karazhan - Chess, Medivh CHEAT: Fury of Medivh, Target Horde
+        case 39344:                                 // Karazhan - Chess, Medivh CHEAT: Fury of Medivh, Target Alliance
+        case 39992:                                 // Needle Spine Targeting (BT, Warlord Najentus)
+        case 40869:                                 // Fatal Attraction (BT, Mother Shahraz)
+        case 41303:                                 // Soul Drain (BT, Reliquary of Souls)
+        case 41376:                                 // Spite (BT, Reliquary of Souls)
+        case 51904:                                 // Summon Ghouls On Scarlet Crusade
+        case 54522:                                 // Summon Ghouls On Scarlet Crusade
+        case 60936:                                 // Surge of Power (h) (Malygos)
+        case 61693:                                 // Arcane Storm (Malygos)
+        case 62477:                                 // Icicle (h) (Ulduar, Hodir)
+        case 63981:                                 // StoneGrip (h) (Ulduar, Kologarn)
+        case 64598:                                 // Cosmic Smash (h) (Ulduar, Algalon)
+        case 64620:                                 // Summon Fire Bot Trigger (Ulduar, Mimiron) hits npc 33856
+        case 70814:                                 // Bone Slice (ICC, Lord Marrowgar, heroic)
+        case 72095:                                 // Frozen Orb (h) (Vault of Archavon, Toravon)
+        case 72089:                                 // Bone Spike Graveyard (Icecrown Citadel, Lord Marrowgar) 25 man
+        case 70826:
+        case 73143:
+        case 73145:
+            sp->MaxTargets = 3;
+            break;
+        case 37676:                                 // Insidious Whisper (SSC, Leotheras the Blind)
+        case 38028:                                 // Watery Grave (SSC, Morogrim Tidewalker)
+        case 46650:                                 // Open Brutallus Back Door (SWP, Felmyst)
+        case 67757:                                 // Nerubian Burrower (Mode 3) (ToCrusader, Anub'arak)
+        case 71221:                                 // Gas spore (Mode 1) (ICC, Festergut)
+            sp->MaxTargets = 4;
+            break;
+        case 30843:                                 // Enfeeble (Karazhan, Prince Malchezaar)
+        case 40243:                                 // Crushing Shadows (BT, Teron Gorefiend)
+        case 42005:                                 // Bloodboil (BT, Gurtogg Bloodboil)
+        case 45641:                                 // Fire Bloom (SWP, Kil'jaeden)
+        case 55665:                                 // Life Drain (h) (Naxx, Sapphiron)
+        case 58917:                                 // Consume Minions
+        case 64604:                                 // Nature Bomb (Ulduar, Freya)
+        case 67076:                                 // Mistress' Kiss (Mode 1) (ToCrusader, Jaraxxus)
+        case 67078:                                 // Mistress' Kiss (Mode 3) (ToCrusader, Jaraxxus)
+        case 67700:                                 // Penetrating Cold (25 man)
+        case 68510:                                 // Penetrating Cold (25 man, heroic)
+            sp->MaxTargets = 5;
+            break;
+        case 61694:                                 // Arcane Storm (h) (Malygos)
+            sp->MaxTargets = 7;
+            break;
+        case 38054:                                 // Random Rocket Missile
+            sp->MaxTargets = 8;
+            break;
+        case 54098:                                 // Poison Bolt Volley (h) (Naxx, Faerlina)
+        case 54835:                                 // Curse of the Plaguebringer (h) (Naxx, Noth the Plaguebringer)
+            sp->MaxTargets = 10;
+            break;
+        case 25991:                                 // Poison Bolt Volley (AQ40, Pincess Huhuran)
+            sp->MaxTargets = 15;
+            break;
+        case 61916:                                 // Lightning Whirl (Ulduar, Stormcaller Brundir)
+            sp->MaxTargets = urand(2, 3);
+            break;
+        case 46771:                                 // Flame Sear (SWP, Grand Warlock Alythess)
+            sp->MaxTargets = urand(3, 5);
+            break;
+        case 63482:                                 // Lightning Whirl (h) (Ulduar, Stormcaller Brundir)
+            sp->MaxTargets = urand(3, 6);
+            break;
+
         //Spells using Aura 109
         case 50040:
         case 50041:
